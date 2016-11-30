ea5df0ef19c55509c8e800f5aa667dab0b894845
Jiu Liancheng <emlcjnil@gmail.com>
2016-11-30 21:27:37 +0800

    improve code

--- query/index.js
+++ query/index.js
@@ -8,12 +8,10 @@ const {query: localQuery} = require('./localQuery');
 const {query: remoteQuery} = require('./remoteQuery');
 
 module.exports = function query(type, domain) {
-  return new Promise((resolve, reject) => {
-    let record = localQuery(type, domain);
-    if (record.length) {
-      resolve(record);
-    }
+  let record = localQuery(type, domain);
+  if (record.length) {
+    return Promise.resolve(record);
+  }
 
-    remoteQuery(type, domain).then(resolve, reject);
-  })
+  return remoteQuery(type, domain);
 };
