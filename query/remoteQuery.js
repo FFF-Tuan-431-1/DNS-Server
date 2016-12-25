64790fed0854feb62d36457b0f266ab429e95097
WayneHan <1830261482@qq.com>
2016-12-25 15:15:37 +0800

    rewrite remoteQuery && add learnFromResult

--- query/remoteQuery.js
+++ query/remoteQuery.js
@@ -1,14 +1,32 @@
 const dns = require('dns');
+var packet = require('dns-packet');
+var dgram = require('dgram');
+var {learn: learnFromResult} = require('./localQuery')
+
+var socket = dgram.createSocket('udp4');
 
 function query(type, name) {
   return new Promise((resolve, reject) => {
-    dns.resolve(name, type, (err, result) => {
-      if (err) {
-        return reject(err)
-      }
-      console.info(`Found query ${type}:${name} at remote`);
-      resolve(result.map(v => ({type, name, data: v})))
+    var buf = packet.encode({
+      type: 'query',
+      id: 1300,
+      flags: packet.RECURSION_DESIRED,
+      questions: [{
+        type: type,
+        name: name
+      }]
     })
+
+    socket.once('message', (msg) => {
+      const resolvedPacket = packet.decode(msg)
+      console.log(resolvedPacket)
+      resolve(resolvedPacket.answers.map(v => ({
+        type: v.type, 
+        name: v.name,
+        data: v.data
+      })))
+    })
+    socket.send(buf, 0, buf.length, 53, '8.8.8.8');
   })
 }
 
